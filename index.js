03dcc822144019917fd1325d3b52d7c7e3205734
Val <vdecyatnik@gmail.com>
2021-05-31 13:02:50 -0700

    o auth finished

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
 
-app.get("/",(req, res) => {
-    res.send({ hi: "there"});
+require("./models/User");
+require("./services/passport");
 
-
-
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
+const app = express();
 
+//middleware
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`App is running on port ${ PORT }`);
+  console.log(`App is running on port ${PORT}`);
 });
